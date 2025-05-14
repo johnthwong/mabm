f51c87f510890cea36461a3e2e1d207e537e6bad
John <wongthjohn@gmail.com>
2025-05-14 03:04:46 -0400

    clean import

--- model.py
+++ model.py
@@ -2,11 +2,7 @@
 # Has a large collection of mathematical functions to operate on these arrays.
 import numpy as np
 
-# Data manipulation and analysis.
-import pandas as pd
-
 # Data visualization tools.
-import seaborn as sns
 import mesa
 import random
 
